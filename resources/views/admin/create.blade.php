b703202d0ad13691f0320411b75789cff7ff670f
RavshanovUsmonbek <36451884+RavshanovUsmonbek@users.noreply.github.com>
2018-05-20 16:13:49 +0500

    create.blade.php changed

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -2,16 +2,19 @@
 
 @section('content')
     @include('partials.errors')
+    @if(Session::has('info'))
+        <div class="row">
+            <div class="col-md-12">
+                <p class="alert alert-info">{{ Session::get('info') }}</p>
+            </div>
+        </div>
+    @endif
     <div class="row">
         <div class="col-md-12">
-            <form action="{{ route('admin.create') }}" method="post">
+            <form action="{{ route('admin.tag.store') }}" method="post">
                 <div class="form-group">
-                    <label for="title">Title</label>
-                    <input type="text" class="form-control" id="title" name="title">
-                </div>
-                <div class="form-group">
-                    <label for="content">Content</label>
-                    <input type="text" class="form-control" id="content" name="content">
+                    <label for="tag">Tag</label>
+                    <input type="text" class="form-control" id="tag" name="tag">
                 </div>
                 {{ csrf_field() }}
                 <button type="submit" class="btn btn-primary">Submit</button>
